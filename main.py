8f1974c3bb9432b860cd4aa50b9bd4b42ffe4f91
MD Habibur Rahman <165423764+md-habibur-rahman-mahdi@users.noreply.github.com>
2024-04-01 10:56:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,151 @@
+import requests
+import re
+import csv
 import os
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-from selenium.common.exceptions import NoSuchElementException
+from urllib.parse import quote
 
-chromedriver_path = 'chromedriver.exe'
+linkedin_email = "" 
+linkedin_password = "" 
 
-os.environ['PATH'] += os.pathsep + os.path.dirname(chromedriver_path)
+target_company_link = "https://www.linkedin.com/company/johnson-&-johnson/"
 
-def extract_email(driver):
-    try:
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div/div[2]/section/div/section[2]/div/a")))
-        
-        # Find the email element within the contact modal using XPath
-        email_element = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[2]/section/div/section[2]/div/a")
+class LinkedIn:
 
-        # Extract the email text
-        email = email_element.text
-        
-        return email
-    except NoSuchElementException:
-        return None
+    def __init__(self):
+        self.s = requests.Session()
+        self.headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36 OPR/67.0.3575.97"
+            }
 
-# Function to log in to LinkedIn
-def login_to_linkedin(driver, email, password):
-    try:
-        # Navigate to the LinkedIn login page
-        driver.get("https://www.linkedin.com/login")
-        time.sleep(1)
-        # Enter the email and password
-        email_field = driver.find_element(By.ID, "username")
-        email_field.send_keys(email)
-        time.sleep(1)
-        password_field = driver.find_element(By.ID, "password")
-        password_field.send_keys(password)
-        time.sleep(1)
-        # Click the sign-in button
-        driver.find_element(By.XPATH, "//button[text()='Sign in']").click()
-        time.sleep(1)
-        # Wait for the login to complete
-        WebDriverWait(driver, 10).until(EC.url_contains("https://www.linkedin.com/feed/"))
-        
-    except Exception as e:
-        print("Error logging in to LinkedIn:", e)
-
-# Configure Selenium options
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
-# options.add_argument('--headless')  # Optional: Run in headless mode to hide browser window
-
-# Initialize WebDriver
-driver = webdriver.Chrome(options=options)
-
-# Credentials for logging in to LinkedIn
-linkedin_email = "vehini7842@laymro.com"
-linkedin_password = "4b>nf=624O7J"
-
-# Log in to LinkedIn
-login_to_linkedin(driver, linkedin_email, linkedin_password)
-
-# Read profile URLs from CSV using pandas
-df = pd.read_csv('followers.csv')
-profile_urls = df['profileUrl'].tolist()
-
-# Create empty lists to store the output
-output_urls = []
-output_emails = []
-
-# Loop through each profile URL
-for url in profile_urls:
-    try:
-        # Visit the profile URL
-        driver.get(url)
-
-        time.sleep(2)
-        
-        # Wait for the "Contact Info" element to be clickable
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/div[2]/span[2]/a')))
-
-        # Click on the "Contact Info" element
-        contact_info_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/div[2]/span[2]/a')
-        contact_info_button.click()
-
-        # Extract email information from the contact modal
-        email = extract_email(driver)
-        time.sleep(1)
-        
-        # Append the URL and email to the output lists
-        output_urls.append(url)
-        if email:
-            output_emails.append(email)
+    
+    def login(self,email,password):
+        try:
+            sc = self.s.get("https://www.linkedin.com/login", headers=self.headers).text
+        except:
+            return False
+        csrfToken = sc.split('csrfToken" value="')[1].split('"')[0]
+        sid = sc.split('sIdString" value="')[1].split('"')[0]
+        pins = sc.split('pageInstance" value="')[1].split('"')[0]
+        lcsrf = sc.split('loginCsrfParam" value="')[1].split('"')[0]
+        data = {
+            'csrfToken': csrfToken,
+            'session_key': email,
+            'ac': '2',
+            'sIdString': sid,
+            'parentPageKey': 'd_checkpoint_lg_consumerLogin',
+            'pageInstance': pins,
+            'trk': 'public_profile_nav-header-signin',
+            'authUUID': '',
+            'session_redirect': 'https://www.linkedin.com/feed/',
+            'loginCsrfParam': lcsrf,
+            'fp_data': 'default',
+            '_d': 'd',
+            'showGoogleOneTapLogin': 'true',
+            'controlId': 'd_checkpoint_lg_consumerLogin-login_submit_button',
+            'session_password': password,
+            'loginFlow': 'REMEMBER_ME_OPTIN'
+            }
+        try:
+            after_login = self.s.post("https://www.linkedin.com/checkpoint/lg/login-submit",headers=self.headers,data=data).text
+        except:
+            return False
+        is_logged_in = after_login.split('<title>')[1].split('</title>')[0]
+        if is_logged_in == "LinkedIn":
+            return True
         else:
-            output_emails.append("No email found")
-        
-    except Exception as e:
-        print(f"Error processing profile URL: {url}")
-        print(e)
+            return False
 
-# Create a DataFrame from the output lists
-output_df = pd.DataFrame({"Profile URL": output_urls, "Email": output_emails})
+    def bulkScan(self, profiles):
+        all_emails = []
+        for profile in profiles:
+            profile = profile + "/detail/contact-info/"
+            sc = self.s.get(profile, headers=self.headers, allow_redirects=True).text
+            emails_found = re.findall(r'[a-zA-Z0-9\.\-\_i]+@[\w.]+',sc)
+            all_emails.extend(emails_found)
+        return all_emails
 
-# Write the DataFrame to a CSV file (overwrite mode)
-output_df.to_csv("scraped_data.csv", mode='w', index=False)
+    def singleScan(self, profile):
+        profile = profile + "/detail/contact-info/"
+        sc = self.s.get(profile, headers=self.headers, allow_redirects=True).text
+        emails_found = re.findall(r'[a-zA-Z0-9\.\-\_i]+@[\w.]+',sc)
+        return emails_found
 
-# Quit the WebDriver
-driver.quit()
+    def saveRecord(self, data):
+        with open('leads.csv', mode='a+', encoding='utf-8', newline='') as csvFile:
+            fieldnames = ["Profile Link", "Full Name", "Headline", "Country"]
+            writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+            if os.stat('leads.csv').st_size == 0:
+                writer.writeheader()
+            writer.writerow({"Profile Link":data[0], "Full Name":data[1], "Headline":data[2], "Country":data[3]})
+
+    def saveEmail(self, email):
+        with open('emails.csv', mode='a+', encoding='utf-8') as emFile:
+            if os.stat('emails.csv').st_size == 0:
+                emFile.write("Email\n")
+            emFile.write(email + "\n")
+
+    def listProfiles(self, company_id, page_no, need_count=False):
+        resp = self.s.get('https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%22{}%22%5D&origin=COMPANY_PAGE_CANNED_SEARCH&page={}'.format(company_id, page_no), headers=self.headers).text
+        token = self.s.cookies.get_dict().get('JSESSIONID').replace('"','')
+        headers = {
+            'csrf-token': token,
+            'referer': 'https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%22{}%22%5D&origin=COMPANY_PAGE_CANNED_SEARCH&page={}'.format(company_id, page_no),
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+        }
+        resp = self.s.get('https://www.linkedin.com/voyager/api/search/blended?count=10&filters=List(currentCompany-%3E{},resultType-%3EPEOPLE)&origin=COMPANY_PAGE_CANNED_SEARCH&q=all&queryContext=List(spellCorrectionEnabled-%3Etrue)&start={}'.format(company_id,(int(page_no)-1) * 10), headers=headers).json()
+        profiles = resp.get('elements')[0].get('elements')
+        all_profile_links = []
+        if need_count:
+            page_count = resp.get('paging').get('total')
+        for profile in profiles:
+            person_name = profile.get('title').get('text')
+            profile_link = profile.get('navigationUrl')
+            headline = profile.get('headline').get('text')
+            country = profile.get('subline').get('text')
+            data = []
+            data.append(profile_link)
+            data.append(person_name)
+            data.append(headline)
+            data.append(country)
+            self.saveRecord(data)
+        if need_count:
+            return all_profile_links, page_count
+        else:
+            return all_profile_links
+
+    def getCompanyID(self, company_link):
+        try:
+            company_username = company_link.split('.com/company/')[1].replace('/','')
+        except:
+            print("Use company link like https://www.linkedin.com/company/unilever/ only!")
+            return None
+        resp = self.s.get(company_link, headers=self.headers).text
+        token = self.s.cookies.get_dict().get('JSESSIONID').replace('"','')
+        headers = {
+            'csrf-token': token,
+            'referer': 'https://www.linkedin.com/',
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+        }
+        api_link = 'https://www.linkedin.com/voyager/api/organization/companies?decorationId=com.linkedin.voyager.deco.organization.web.WebCompanyStockQuote-2&q=universalName&universalName={}'.format(quote(company_username))
+        resp = self.s.get(api_link, headers=headers).json()
+        company_id = resp.get('elements')[0].get('entityUrn').split(':')[-1]
+        return company_id
+
+if __name__ == "__main__":
+    connection = LinkedIn()
+    login_state = connection.login(linkedin_email, linkedin_password)
+    if login_state:
+        print("Logged in to LinkedIn!")
+        company_id = connection.getCompanyID(target_company_link)
+        if company_id is not None:
+            print("Collecting all company member profiles upto 10000!")
+            profile_list, page_count = connection.listProfiles(company_id, 1, True)
+            for page_no in range(2,page_count+1):
+                profile_list.extend(connection.listProfiles(company_id, page_no))
+            print("Profile list collected and saved! Extracting emails ...")
+            all_emails = connection.bulkScan(profile_list)
+            for email in all_emails:
+                print(email)
+                connection.saveEmail(email)
+    else:
+        print("Unable to login to LinkedIn!")
